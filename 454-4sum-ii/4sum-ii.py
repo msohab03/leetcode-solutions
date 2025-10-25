015a198d52564c0189d3492940cd646dc1633770
Mohammed Sohab <msohab03@gmail.com>
2025-10-24 22:41:08 -0700

    Time: 510 ms (8.28%) | Memory: 18.3 MB (14.61%) - LeetSync

---
+++ 454-4sum-ii/4sum-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        # tuples = (i, j, k, l) pairs = we just need the amount of pairs
+        # split into a, b and search c, d complement
+        ab = {}
+
+        count = 0
+        # we take the approach of a + b = -(c + d) we find complement in the two hashmap pairs
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] + nums2[j] in ab:
+                    ab[nums1[i] + nums2[j]] += 1
+                else:
+                    ab[nums1[i] + nums2[j]] = 1
+        
+        for k in range(len(nums3)):
+            for l in range(len(nums4)):
+                if -(nums3[k] + nums4[l]) in ab:
+                    count += ab[-(nums3[k] + nums4[l])]
+               
+        print(ab)
+        return count
