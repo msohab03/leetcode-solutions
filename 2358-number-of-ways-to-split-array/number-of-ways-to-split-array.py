304bbf250adc387fab46a840d685c5529755bf34
Mohammed Sohab <msohab03@gmail.com>
2025-11-03 00:24:28 -0800

    Time: 91 ms (19.78%) | Memory: 33.4 MB (19.78%) - LeetSync

---
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        valid_splits = 0
+        prefix = [nums[0]]
+        n = len(nums)
+        for i in range(1, n):
+            prefix.append(nums[i] + prefix[i - 1])
+        
+        for i in range(len(prefix)):
+            if 0 <= i < n - 1:
+                if prefix[i] >= prefix[-1] - prefix[i]:
+                    valid_splits += 1
+
+        return valid_splits
